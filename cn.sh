4a24e835a49bfbc982467bae9a00fa0a0cee5d51
forkyyy <67451588+forkyyy@users.noreply.github.com>
2023-03-29 10:36:04 -0300

    Update cn.sh

--- cn.sh
+++ cn.sh
@@ -13,7 +13,4 @@ cat wsd.unfiltered | sed 's/,/ /g'| awk '$2 >= 800' > CN_WSD
 zmap -p37810 --output-filter='sport=37810' -Mudp --probe-args=file:dvr.pkt -f "saddr,udp_pkt_size" -w cn.zone -o dvr.unfiltered -T11 -i vlan159
 cat dvr.unfiltered | sed 's/,/ /g'| awk '$2 >= 600' > CN_DVR
 
-zmap -p37020 --output-filter='sport=37020' -Mudp --probe-args=file:sadp.pkt -f "saddr,udp_pkt_size" -w cn.zone -o sadp.unfiltered -T11 -i vlan159
-cat sadp.unfiltered | sed 's/,/ /g'| awk '$2 >= 800' > CN_SADP
-
 echo "done!"
