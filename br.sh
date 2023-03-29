ce0ef832b2bd920d472290c6240bae6823c41458
forkyyy <67451588+forkyyy@users.noreply.github.com>
2023-03-29 10:38:02 -0300

    Update br.sh

--- br.sh
+++ br.sh
@@ -1,6 +1,3 @@
-zmap -p53 --output-filter='sport=53' -Mudp --probe-args=file:dns.pkt -f "saddr,udp_pkt_size" -w br.zone -o dns.unfiltered -T11 -i vlan159
-cat dns.unfiltered | sed 's/,/ /g'| awk '$2 >= 3000' > BR_DNS
-
 zmap -p123 --output-filter='sport=123' -Mudp --probe-args=file:ntp.pkt -f "saddr,udp_pkt_size" -w br.zone -o ntp.unfiltered -T11 -i vlan159
 cat ntp.unfiltered | sed 's/,/ /g'| awk '$2 >= 400' > BR_NTP
 
